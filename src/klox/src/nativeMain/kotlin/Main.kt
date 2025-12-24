3e659e41d1996f4f11bfe1742615fa970ad4ab0b
Bowei Liu <liubowei@gmail.com>
2025-12-24 00:38:53 -0800

    hmmm why dont we need modules here

--- src/klox/src/nativeMain/kotlin/Main.kt
+++ src/klox/src/nativeMain/kotlin/Main.kt
@@ -54,26 +54,3 @@ fun runPrompt(): Int {
     errors.forEach { it.reportToStdout() }
   }
 }
-
-data class InterpreterError(
-  val locationLineNo: Int,
-  val locationFile: String,
-  val message: String,
-)
-
-fun InterpreterError.reportToStdout(): Unit {
-  println("Error: ${this.message} at ${this.locationFile}:${this.locationLineNo}")
-}
-
-fun run(ss: String, sourceFname: String): List<InterpreterError> {
-  return sequence<InterpreterError> {
-    // TODO: start scanning
-
-    // test yielding data/errors
-    this.test_here({ yield(InterpreterError(-1, sourceFname, ss)) })
-  }.toList<InterpreterError>()
-}
-
-suspend fun SequenceScope<InterpreterError>.test_here(cb: suspend SequenceScope<InterpreterError>.() -> Unit): Unit {
-  this.cb()
-}
