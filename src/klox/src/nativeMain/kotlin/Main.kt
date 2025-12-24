d41758473afbfe970287e10c700c590f3f14db8b
Bowei Liu <liubowei@gmail.com>
2025-12-23 22:58:36 -0800

    get the cli up

--- src/klox/src/nativeMain/kotlin/Main.kt
+++ src/klox/src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,32 @@
-fun main() {
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>): Unit {
   println("hello from native kotlin rebuild here")
+  println(args.joinToString(","))
+
+  var retCode: Int;
+  if (args.size > 1) {
+    println("Usage: klox [script]")
+    retCode = 64
+  } else if (args.size == 1 ) {
+    retCode = runFile(args[0])
+  } else {
+    retCode = runPrompt()
+  }
+  exitProcess(retCode)
+  return
+}
+
+fun runFile(fname: String): Int {
+  return 0
+}
+
+fun runPrompt(): Int {
+  while (true) {
+    val ln = readln()
+    println(ln)
+    if (ln.length == 0) {
+      return 0
+    }
+  }
 }
