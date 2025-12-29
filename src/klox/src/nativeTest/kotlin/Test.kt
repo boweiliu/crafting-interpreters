103aea12a878682626ec22ba7ad72348ed7f474c
Bowei Liu <liubowei@gmail.com>
2025-12-29 11:24:47 -0800

    ok

--- src/klox/src/nativeTest/kotlin/Test.kt
+++ src/klox/src/nativeTest/kotlin/Test.kt
@@ -1,5 +1,7 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import io.kotest.matchers.*
+import io.kotest.matchers.collections.*
 
 @Test
 fun test1() {
@@ -14,3 +16,20 @@ fun test2() {
   println("test")
   assertEquals(1, 1, "good this should pass")
 }
+
+@Test
+fun testRunLexerJustRuns() {
+  val (tokenSeq, errorsSoFar) = runLexer("1 + 2", "<stdin>")
+  errorsSoFar.shouldHaveSize(0)
+}
+
+
+@Test
+fun testRunLexerRunsLazily() {
+  val (tokenSeq, errorsSoFar) = runLexer("1 + 2\"", "<stdin>")
+  errorsSoFar.shouldHaveSize(0)
+  tokenSeq.toList()
+  errorsSoFar.shouldHaveSize(2)
+}
+
+
