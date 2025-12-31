959b9fed7fd99fa103fbf6b5ddc12b0501c3dc48
Bowei Liu <liubowei@gmail.com>
2025-12-31 13:00:24 -0800

    test still pass

--- src/klox/src/nativeMain/kotlin/sequence/Sequence.kt
+++ src/klox/src/nativeMain/kotlin/sequence/Sequence.kt
@@ -3,54 +3,6 @@ package sequence
 import kotlin.coroutines.*
 import kotlin.experimental.*
 
-
-/**
- * Example of a sequence that receives yields().
- */
-
-@RestrictsSuspension
-interface SequenceScope<in T> {
-    suspend fun yield1(value: T)
-}
-
-@OptIn(kotlin.experimental.ExperimentalTypeInference::class)
-fun <T> mySequence(
-  @BuilderInference block: suspend SequenceScope<T>.() -> Unit
-): Sequence<T> = 
-    SequenceCoroutine<T>().apply {
-        nextStep = block.createCoroutine(receiver = this, completion = this)
-    }.asSequence()
-
-private class SequenceCoroutine<T>: AbstractIterator<T>(), SequenceScope<T>, Continuation<Unit> {
-    lateinit var nextStep: Continuation<Unit>
-
-    override fun computeNext() {
-      nextStep.resume(Unit)
-    }
-
-    override val context: CoroutineContext get() = EmptyCoroutineContext
-
-    override fun resumeWith(result: Result<Unit>) {
-        result.getOrThrow() // bail out on error
-        done()
-    }
-
-    // Generator implementation
-    override suspend fun yield1(value: T) {
-        setNext(value)
-        return suspendCoroutine { cont -> nextStep = cont }
-    }
-}
-
-@OptIn(kotlin.experimental.ExperimentalTypeInference::class)
-fun <T> myEmitter(
-  data: Sequence<Int>,
-  @BuilderInference block: suspend SequenceAndEmitterScope<T>.() -> Unit
-): DuoIterator<Int, T> = 
-    SequenceAndEmitterCoroutine<T>(data.iterator()).apply {
-        nextStep = block.createCoroutine(receiver = this, completion = this)
-    }
-
 interface DuoIterator<A, T> {
   fun hasStarted(): Boolean
   fun start(): Unit
@@ -69,62 +21,8 @@ interface DuoIterator<A, T> {
     }
     return seq.iterator()
   }
-
-  // Temp for testing/uncompile
-  fun iterator(): Iterator<T>
 }
 
-
-@RestrictsSuspension
-interface SequenceAndEmitterScope<in T> {
-    suspend fun yield1(value: T)
-    suspend fun obtain2(i: Int): Unit
-    suspend fun duoYield(i: Int): Int
-}
-
-private class SequenceAndEmitterCoroutine<T>(val myData: Iterator<Int>): AbstractIterator<T>(), SequenceAndEmitterScope<T>, Continuation<Unit>, DuoIterator<Int, T> {
-    lateinit var nextStep: Continuation<Unit>
-
-    private var didStart = false
-    override fun hasStarted() = didStart
-    override fun start() {
-      didStart = true
-    }
-    override fun iterator() = this
-    override fun canSend() = hasNext()
-    override fun send(aa: Int): T {
-      // calls the continuation...?
-      TODO()
-    }
-
-    override fun computeNext() {
-      nextStep.resume(Unit)
-    }
-
-    override val context: CoroutineContext get() = EmptyCoroutineContext
-
-    override fun resumeWith(result: Result<Unit>) {
-        result.getOrThrow() // bail out on error
-        done()
-    }
-
-    // Generator implementation
-    override suspend fun yield1(value: T) {
-        setNext(value)
-        return suspendCoroutine { cont -> nextStep = cont }
-    }
-
-    // Generator implementation
-    override suspend fun obtain2(i: Int): Unit {
-        TODO()
-        // return myData.next()
-    }
-
-    override suspend fun duoYield(i: Int): Int {
-        return 0
-        // return myData.next()
-    }
-}
 
 
 @OptIn(kotlin.experimental.ExperimentalTypeInference::class)
@@ -141,17 +39,7 @@ interface DuoSequenceScope<in T, out A> {
     suspend fun initCoYield(): A
     suspend fun initCoYield(ig: T?): A = initCoYield()
     // suspend fun coYield(ig: T?): A // convienient for duoYield but without the type check
-    // suspend fun finalYield(value: T): Unit
 }
-
-
-// interface DuoIterator<A, T> {
-//   fun start(): Unit
-//   fun canSend(): Boolean
-//   fun send(aa: A): T
-//   // Temp for testing/uncompile
-//   fun iterator(): Iterator<T>
-// }
 
 private class DuoSequenceCoroutine<A, T>:
   DuoSequenceScope<T, A>,
@@ -164,22 +52,17 @@ private class DuoSequenceCoroutine<A, T>:
     lateinit var firstStep: Continuation<Unit>
     private lateinit var nextStep: Continuation<A>
 
-    override fun iterator() = TODO() // legacy thing, unneeded
     override fun hasStarted(): Boolean = didStart
+    override fun canSend() = !didFinish
+
     override fun start() {
       didStart = true
       firstStep.resume(Unit)
-      // TODO() ??
     }
-    override fun canSend() = !didFinish
+
     override fun send(aa: A): T {
       nextStep.resume(aa)
-      val result: T = bufferedValue!!
-      bufferedValue = null
-      return result
-
-      // calls the continuation...? or...?
-      // return suspendCoroutine { cont -> nextStep = cont ; tt }
+      return bufferedValue!!.also { bufferedValue = it }
     }
 
     // Generator implementation
@@ -187,12 +70,12 @@ private class DuoSequenceCoroutine<A, T>:
       bufferedValue = value
       return suspendCoroutine { cont -> nextStep = cont }
     }
+
     override suspend fun initCoYield(): A {
       return suspendCoroutine { cont -> nextStep = cont }
     }
-    // override suspend fun finalYield(value: T): Unit { TODO() }
 
-
+    // Continuation/coroutine implementation
     override val context: CoroutineContext get() = EmptyCoroutineContext
 
     override fun resumeWith(result: Result<T>): Unit {
--- src/klox/src/nativeTest/kotlin/sequence/SequenceTest.kt
+++ src/klox/src/nativeTest/kotlin/sequence/SequenceTest.kt
@@ -4,68 +4,13 @@ import kotlin.test.*
 import io.kotest.matchers.*
 import io.kotest.matchers.collections.*
 
-
-
 @Test
-fun my() {
-  val acc: MutableList<Int> = mutableListOf(-1)
-  val s = mySequence<Int> { 
-    for (i in 1..3) {
-      acc.add(i * 10)
-      yield1(i)
-    }
-  }
-  val it = s.iterator()
-  acc.toList().shouldBe(listOf(-1))
-  it.hasNext()
-  acc.toList().shouldBe(listOf(-1,10))
-  it.next().shouldBe(1)
-  acc.toList().shouldBe(listOf(-1,10))
-
-  it.next().shouldBe(2)
-  acc.toList().shouldBe(listOf(-1,10,20))
-  it.next()
-  acc.toList().shouldBe(listOf(-1,10,20,30))
-  it.hasNext().shouldBe(false)
-}
-
-@Test
-@Ignore
-fun obtain() {
-  val source: Sequence<Int> = listOf(1,2,3,4,5).asSequence()
-  val acc: MutableList<Int> = mutableListOf(-1)
-
-  val s = myEmitter<Int>(source) {
-    while (true) {
-      break
-      // val it = obtain2(42)
-      // if (it >= 4) break@while
-      // acc.add(it * 10)
-      // yield1(it)
-    }
-  }
-
-  val it = s.iterator()
-  acc.toList().shouldBe(listOf(-1))
-  it.hasNext()
-  acc.toList().shouldBe(listOf(-1,10))
-  it.next()
-  acc.toList().shouldBe(listOf(-1,10))
-  it.next()
-  acc.toList().shouldBe(listOf(-1,10,20))
-  it.next()
-  acc.toList().shouldBe(listOf(-1,10,20,30))
-  it.hasNext().shouldBe(false)
-}
-
-@Test
-fun duoSequenceCanBeCalled() {
+fun duoIteratorCanBeCalled() {
   val myDuoSequence = object : DuoIterator<Int, String> {
     var state: Int = 0
     override fun hasStarted() = false
     override fun start() { }
     override fun canSend() = true
-    override fun iterator() = TODO()
     override fun send(aa: Int): String {
       state += aa
       return state.toString()
